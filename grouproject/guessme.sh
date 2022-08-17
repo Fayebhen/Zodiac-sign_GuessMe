a6af4f24320c4a04c3211a2e83835fb9f22b8c3d
faye <fbhenry08@gmail.com>
2022-08-17 16:17:30 -0400

    update to main

--- grouproject/guessme.sh
+++ grouproject/guessme.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-======================================================
-#Script		:
+############################################################################
+#Script name            :guessme.sh
+#Description            :Zodiac sign generator with personality traits
+#Authors                :Faye Beerom-Henry; Brayan Molina; Sammy Cespedes;
+#			 Kerri Smith; Junior Cornibert
+#Project Management 
+#Platform		:GitHub
+#Date                   :August 17, 2022
+#Bugs	         	:
+##############################################################################
 
-
-=====================================================
+# This script will generate your zodiac sign and make your day!
+# Welcome!!!
+read -p "Enter your first name:" name
+echo "Hello $name, welcome to Zodiac-GuessMe! Discover more about you." 
 
